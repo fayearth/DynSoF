6e8d94080007ecc161b467d901a6a1408a799649
Fayearth <kakazekage@gmail.com>
2021-11-13 21:04:05 +0700

    Update Logger initialization

--- src/main/java/me/andre111/dynamicsf/DynamicSoundFilters.java
+++ src/main/java/me/andre111/dynamicsf/DynamicSoundFilters.java
@@ -39,7 +39,9 @@ public class DynamicSoundFilters implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("Initializing...");
 		Config.loadData();
 		ClientTickEvents.END_CLIENT_TICK.register(FILTER_MANAGER::updateGlobal);
+		LOGGER.info("Initialization complete");
 	}
 }
